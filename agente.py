7eb5743ff3fe40ef96c3e64fe468df45dbc07eda
cedstudiocommerciale-gif <cedstudiocommerciale@gmail.com>
2026-07-18 01:30:03 +0200

    Update agente.py

--- agente.py
+++ agente.py
@@ -7,7 +7,7 @@ from google.genai import types
 # Creiamo l'applicazione server
 app = FastAPI()
 
-# Impostiamo la chiave API (Sostituisci la scritta con la tua vera chiave se la hai, altrimenti lasciala così per ora)
+# Impostiamo la chiave API 
 GEMINI_API_KEY = os.environ.get("GEMINI_API_KEY", "AQ.Ab8RN6KLRjuCkD9v8uuJVVlHxvLPxyL1OU18FBlwv1R2WisdjQ")
 client = genai.Client(api_key=GEMINI_API_KEY)
 
@@ -19,13 +19,14 @@ class RichiestaUtente(BaseModel):
 @app.post("/parla-con-agente")
 async def rispondi_al_messaggio(richiesta: RichiestaUtente):
     try:
-        response = client.models.generate_content
-           model='gemini-3.5-flash',
+        response = client.models.generate_content(
+            model='gemini-3.5-flash',
             contents=richiesta.messaggio,
-           config=types.GenerateContentConfig(
-                system_instruction="Sei l'Agente IA principale di un software gestionale e contabile. Il tuo scopo è aiutare l'utente con fatturazione,riconciliazione bancaria, calcoli IVA, bilanci, normative fiscali e gestione aziendale. Rispondi in modo rigoroso, professionale e analitico. Rifiutati cortesemente di rispondere a domande non pertinenti all'ambito economico o contabile.",
-                temperature=0.7,
+            config=types.GenerateContentConfig(
+                system_instruction="Sei l'Agente IA principale di un software gestionale e contabile. Il tuo scopo è aiutare l'utente con fatturazione, calcoli IVA, bilanci, normative fiscali e gestione aziendale. Rispondi in modo rigoroso, professionale e analitico. Rifiutati cortesemente di rispondere a domande non pertinenti all'ambito economico o contabile.",
+                temperature=0.7
             )
+        )
         return {"risposta_agente": response.text}
         
     except Exception as e:
